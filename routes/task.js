e63e072d872de67741669f6c04cfd381ef1acc61
Astitva877 <ankurpatle18@gmail.com>
2023-06-11 23:53:13 +0530

    update api added

--- routes/task.js
+++ routes/task.js
@@ -20,6 +20,21 @@ router.post("/create", async (req, res) => {
   }
 });
 
+//UPDATE
+router.put("/update/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //DELETE
 router.delete("/:id", async (req, res) => {
